b34d1f083f0e7b3e5a827e91c35ddbc7888cf24a
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-29 12:39:08 +0700

    change modal text

--- web-interface/src/components/SetTargetHost.modal.tsx
+++ web-interface/src/components/SetTargetHost.modal.tsx
@@ -5,12 +5,15 @@ export const SetTargetHost = () => {
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                            Insert redis connection string
+                            Connect to redis server
                         </h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal"/>
                     </div>
                     <div className="modal-body">
-                        ...
+                    <div className="input-group mb-3">
+                        <span className="input-group-text" id="basic-addon1">redis://</span>
+                        <input type="text" className="form-control" placeholder="username:password@awesome.redis.server:1234" autofocus></input>
+                    </div>
+
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-primary">Connect</button>
