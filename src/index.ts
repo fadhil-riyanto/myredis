1fabda31290715bedf7c2a921ecb7818e598cc4a
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-29 09:56:06 +0700

    hello world graphql

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import path from "path"
-
+import { createHandler } from "graphql-http/lib/use/express"
+import { schema } from "./graphQlSchema"
 
 const preactWorkingDir = path.join(__dirname, "..", "web-interface", "dist")
 
@@ -10,6 +11,7 @@ app.use(express.static(preactWorkingDir))
 app.get("/", (req: Request, res: Response) => {
     res.sendFile(preactWorkingDir + "/index.html")
 }) 
+app.all('/graphql', createHandler({ schema }));
 
 
 app.listen({ port: 8000 });
