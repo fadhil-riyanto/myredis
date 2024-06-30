32d15a301f2b178190ceccd839163df1219847ae
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-30 12:52:30 +0700

    add new method and add new parameters "conntoken"

--- src/graphQlSchema.ts
+++ src/graphQlSchema.ts
@@ -3,6 +3,7 @@ import { buildSchema } from "graphql";
 export const schema = buildSchema(`
     type Query {
       RedisCreateSession(connstr: String): String
-      Get(keys: String): String
+      Get(ConnToken: String, key: String): String
+      Getall(ConnToken: String): [String]
     }
 `)
