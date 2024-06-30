ecbba16e4d6f4baa6cbf16d160002cab64b3219d
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-30 12:53:20 +0700

    add header allow origin, move handler, init session table

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 import express, {Request, Response} from "express";
 import path from "path"
 import { createHandler } from "graphql-http/lib/use/express"
+
 import { schema } from "./graphQlSchema"
 import { redisConnect } from "./operations/connect"
+import { sessionTable } from "./SessionTable"
+import { RedisClientType } from 'redis';
 
-let redisCtx: any = null;
 const preactWorkingDir = path.join(__dirname, "..", "web-interface", "dist")
 const app = express()
+const session = new sessionTable()
 
 const rootValue = {
     async RedisCreateSession({connstr}:{connstr:string}) : Promise<string | null> {
-        redisCtx = await redisConnect(connstr)
+        let redisCtx: any = await redisConnect(connstr)
         
-        return await redisCtx.get("wkaka");
+        return session.gen(redisCtx, connstr)
     },
 
-    async Get({keys}:{keys:string}) {
-        return await redisCtx.get(keys)
+    async Get({ConnToken, key}:{ConnToken:string, key: string}) {
+        return session.get(ConnToken).redisCtx?.get(key)
+    },
+
+    async Getall({ConnToken}:{ConnToken:string, key: string}): Promise<string[] | undefined> {
+        return await session.get(ConnToken).redisCtx?.sendCommand(['KEYS', '*'])
     }
 }
 
@@ -25,9 +32,13 @@ app.get("/", (req: Request, res: Response) => {
     res.sendFile(preactWorkingDir + "/index.html")
 }) 
 
-app.all('/graphql', createHandler({ 
-    schema: schema,
-    rootValue: rootValue
-}));
+app.all('/graphql', (req: Request, res: Response, next: any) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        next()
+    }, createHandler({ 
+        schema: schema,
+        rootValue: rootValue
+    })
+);
 
 app.listen({ port: 8000 });
