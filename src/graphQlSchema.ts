7bd7484e46307a9c6392baa507d7744375dccdb2
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-01 11:02:21 +0700

    add new type to the schema

--- src/graphQlSchema.ts
+++ src/graphQlSchema.ts
@@ -1,9 +1,18 @@
 import { buildSchema } from "graphql";
 
 export const schema = buildSchema(`
+    type DumpType {
+      key: String
+      value: String
+      expire: String
+      type: String
+    }
     type Query {
       RedisCreateSession(connstr: String): String
       Get(ConnToken: String, key: String): String
-      Getall(ConnToken: String): [String]
+      Getall(ConnToken: String): [DumpType]
     }
+
+  
+
 `)
