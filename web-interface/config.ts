8e8a4a2f4f49dd211d0a58f90ee14c289d2f1759
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-02 10:55:45 +0700

    export env for development mode

--- web-interface/config.ts
+++ web-interface/config.ts
@@ -1,10 +1,11 @@
 const MODE = "development"
+const VERSION = "v1.0.0"
 
 let graphqlEndpoint
 if (MODE == "development") {
-    graphqlEndpoint = "http://127.0.0.1:8000/graphql"
+    graphqlEndpoint = "http://127.0.0.1:9000/graphql"
 } else {
     graphqlEndpoint = "/graphql"
 }
 
-export { graphqlEndpoint }
+export { graphqlEndpoint, VERSION, MODE }
