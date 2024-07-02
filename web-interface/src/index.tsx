cc4dfaa0dd80057bb8598e4021491f01f39417ae
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-07-02 10:55:08 +0700

    set theme to dark

--- web-interface/src/index.tsx
+++ web-interface/src/index.tsx
@@ -1,10 +1,9 @@
 import { render } from 'preact';
 import { SetTargetHost } from './components/SetTargetHost.modal'
 
-import * as bootstrap from 'bootstrap'
 import "./style.scss"
 import { useEffect, useState } from 'preact/hooks';
-
+import { Dashboard } from "./dashboard"
 
 export function App() {
 	const [globalConnToken, globalConnTokenset] = useState(null)
@@ -12,8 +11,10 @@ export function App() {
 
 	return (
 		<>
+			<div data-bs-theme="dark" className="bg-dark min-vh-100">
 			<SetTargetHost parentRefTokenset={globalConnTokenset} parentRefToken={globalConnToken} />
-			<h1>hello world</h1>
+			<Dashboard parentRefToken={globalConnToken}/>
+			</div>
 		</>
 	);
 }
